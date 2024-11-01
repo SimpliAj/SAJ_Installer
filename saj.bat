7c6d2fea8450fd1ccafabb9db5489990b93f41fa
Aj <122173112+SimpliAj@users.noreply.github.com>
2024-11-01 21:20:35 +0100

    Update saj.bat

--- saj.bat
+++ saj.bat
@@ -3,35 +3,35 @@ title Program Installation Menu
 
 :: Array of programs to display
 set programs[1]=Google Chrome
-set packages[1]=googlechrome
+set urls[1]=https://dl.google.com/chrome/install/latest/chrome_installer.exe
 set programs[2]=Brave Browser
-set packages[2]=brave
+set urls[2]=https://laptop-updates.brave.com/latest/win/BraveSetup.exe
 set programs[3]=Mozilla Firefox
-set packages[3]=firefox
+set urls[3]=https://download-installer.cdn.mozilla.net/pub/firefox/releases/latest/win64/en-US/Firefox%20Installer.exe
 set programs[4]=Opera Browser
-set packages[4]=opera
+set urls[4]=https://download.opera.com/download/get/?partner=web&opsys=win&next=install&inst=full
 set programs[5]=Discord
-set packages[5]=discord
+set urls[5]=https://dl.discordapp.net/win/latest/DiscordSetup.exe
 set programs[6]=Malwarebytes
-set packages[6]=malwarebytes
+set urls[6]=https://data.malwarebytes.com/file/mb3-setup-consumer
 set programs[7]=Python 3
-set packages[7]=python
+set urls[7]=https://www.python.org/ftp/python/3.9.1/python-3.9.1-amd64.exe
 set programs[8]=Node.js
-set packages[8]=nodejs
+set urls[8]=https://nodejs.org/dist/v16.14.0/node-v16.14.0-x64.msi
 set programs[9]=Visual Studio Code
-set packages[9]=vscode
+set urls[9]=https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user
 set programs[10]=Steam
-set packages[10]=steam
+set urls[10]=https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe
 set programs[11]=Epic Games
-set packages[11]=epicgameslauncher
+set urls[11]=https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download
 set programs[12]=7-Zip
-set packages[12]=7zip
+set urls[12]=https://www.7-zip.org/a/7z1900-x64.exe
 set programs[13]=WinRAR
-set packages[13]=winrar
+set urls[13]=https://www.rarlab.com/rar/winrar-x64-601.exe
 set programs[14]=qBittorrent
-set packages[14]=qbittorrent
+set urls[14]=https://qbittorrent.org/download.php
 set programs[15]=Spotify
-set packages[15]=spotify
+set urls[15]=https://download.scdn.co/SpotifySetup.exe
 
 :: Total number of programs
 set total=15
@@ -46,7 +46,6 @@ echo.
 for /L %%i in (1,1,%total%) do (
     setlocal enabledelayedexpansion
     set program=!programs[%%i]!
-    set package=!packages[%%i]!
     if !selected[%%i]!==1 (
         echo [X] %%i - !program!
     ) else (
@@ -84,9 +83,10 @@ echo Installing selected programs...
 for /L %%i in (1,1,%total%) do (
     setlocal enabledelayedexpansion
     if !selected[%%i]!==1 (
-        set package=!packages[%%i]!
-        echo Installing !package!...
-        choco install !package! -y
+        set program=!programs[%%i]!
+        set url=!urls[%%i]!
+        echo Installing !program!...
+        powershell -Command "Start-Process -FilePath '!url!' -ArgumentList '/S' -Wait"
     )
     endlocal
 )
